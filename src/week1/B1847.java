e94cea9cc90f5578c21da32a01f91f18b6d81a7f
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-07-31 22:10:29 +0900

    update B1847 7/31

---
+++ src/week1/B1847.java
@@ -0,0 +1,45 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class B1847 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		Stack<Integer> st = new Stack<>();
+		
+		int cnt = 1;
+		boolean chk=false;
+		List<String> result = new ArrayList<>();
+		while(n-->0) {
+			int a = Integer.parseInt(br.readLine());
+			while(cnt<=a) {
+				st.push(cnt++);
+				result.add("+");
+			}
+			if( st.peek() != a) {
+				bw.write("No\n");
+				chk=true;
+				break;
+			}
+			st.pop();
+			result.add("-");
+		}
+		if (chk == false){
+			for(String c : result) {
+				bw.write(c+"\n");
+			}
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
