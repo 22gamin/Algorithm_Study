7aa8cb3745957b490ab3b476241212079287f4e4
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-08-03 16:28:28 +0900

    update B2493.java 8/3

---
+++ src/week1/B2493.java
@@ -0,0 +1,43 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import java.util.Stack;
+
+//1 2 3 4 5
+public class B2493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n =Integer.parseInt(br.readLine());
+        Stack<Integer> st = new Stack<Integer>();
+        int[] top = new int[n];
+        int[] total = new int [n];
+
+        String[] str = br.readLine().split(" ");
+        for(int i=0; i<n; i++){
+            top[i] = Integer.parseInt(str[i]);
+            // 들어올떄마다 관리
+
+            // 비거나, 스택에 들어가있는 탑 인덱스의 높이 보다 클경우
+            while(!st.isEmpty() && top[i]>top[st.peek()]){
+                // 수신x
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                // 인덱스 0부터니까 +1
+                total[i] = st.peek()+1;
+            }else{
+                total[i] = 0;
+            }
+            st.push(i);
+        }
+        for(int s : total){
+            System.out.print(s+" ");
+        }
+    }
+}
