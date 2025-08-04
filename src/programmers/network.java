b44fd8d68054e9fe6da5f68b1f1927d3471a91c6
gamin <gamin3182@gmail.com>
2025-08-04 21:33:40 +0900

    8/4 network

---
+++ src/programmers/network.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.*;
+
+public class network {
+
+    class Solution {
+        public int solution(int n, int[][] computers) {
+            boolean[] visited = new boolean[n];
+            int count = 0;
+
+            Queue<Integer> queue = new LinkedList<>();
+
+            //다 넣기
+            for(int i = 0; i<n; i++){
+                if (!visited[i]){
+                    queue.add(i);
+                    visited[i] = true;
+
+                    while(!queue.isEmpty()){
+                        int current = queue.poll();
+
+                        for(int j = 0; j<n; j++){
+                            if (!visited[j] && computers[current][j]==1){
+                                visited[j] = true;
+                                queue.add(j);
+                            }
+                        }
+                    }
+                    count ++;
+                }
+            }
+
+
+
+            return count;
+        }
+    }
+}
