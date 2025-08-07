04bb0b5c5f496fec331a776e94aa0d431d1e02cc
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-08-07 23:44:06 +0900

    2주차/김민종/5문제

---
+++ 2주차/boj_15650_김민종.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class boj_15650_김민종 {
+    static int N, M;
+    static int[] ans;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ans = new int[M];
+
+        fx(0,1);
+    }   
+    
+    static void fx(int depth, int num){
+        if(depth == M){
+            for(int i = 0; i < M; i++){
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i = num; i <= N; i++){
+            ans[depth] = i;
+            fx(depth + 1, i + 1);
+        }
+    }
+}
