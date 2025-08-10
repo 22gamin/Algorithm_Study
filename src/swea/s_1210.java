2624cc879f38a3e6fa04e7b3b02b5c44b93d57fa
gamin <gamin3182@gmail.com>
2025-08-10 22:10:48 +0900

    8/10 swea-5215

--- src/swea/s_1210.java
+++ src/swea/s_1210.java
@@ -5,54 +5,30 @@ import java.util.*;
 
 public class s_1210 {
 
+    static List<int[]> list = new ArrayList<>();
+    static int[] ma = new int[2];
+
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-        for(int t = 1; t <= 10; t++){
-            int T = Integer.parseInt(br.readLine());
-            int[][] graph = new int[100][100];
+        for(int i=1; i<T+1; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());  // 재료의 수
+            int L = Integer.parseInt(st.nextToken());  // 제한 칼로리
 
-            for (int i = 0; i < 100; i++){
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < 100; j++){
-                    graph[i][j] = Integer.parseInt(st.nextToken());
-                }
+            //입력
+            for (int j = 0; j < N; j++){
+                st = new StringTokenizer(br.readLine());
+                int score = Integer.parseInt(st.nextToken());
+                int calories = Integer.parseInt(st.nextToken());
+
+                list.add(new int[]{score,calories});
+
             }
 
-            // 2가 있는 열 위치 찾기
-            int row = 99;
-            int col = 0;
-            for (int i = 0; i < 100; i++) {
-                if (graph[99][i] == 2) {
-                    col = i;
-                    break;
-                }
-            }
-
-            // 위로 올라가기
-            while (row > 0) {
-                // 왼쪽 확인
-                if (col - 1 >= 0 && graph[row][col - 1] == 1) {
-                    while (col - 1 >= 0 && graph[row][col - 1] == 1) {
-                        col--;
-                    }
-                    row--;
-                }
-                // 오른쪽 확인
-                else if (col + 1 < 100 && graph[row][col + 1] == 1) {
-                    while (col + 1 < 100 && graph[row][col + 1] == 1) {
-                        col++;
-                    }
-                    row--;
-                }
-                // 그냥 위로
-                else {
-                    row--;
-                }
-            }
-
-            System.out.println("#" + T + " " + col);
+            // 제한 칼로리 이하의 조합 중 맛 점수가 가장 높은 것 
         }
     }
 }
