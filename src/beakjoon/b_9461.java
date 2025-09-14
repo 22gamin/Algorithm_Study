3ffce52688ab088ad97e3e8428df83a4788d72a5
gamin <gamin3182@gmail.com>
2025-09-15 00:23:13 +0900

    9/9 BOJ 9461

---
+++ src/beakjoon/b_9461.java
@@ -0,0 +1,37 @@
+package beakjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class b_9461 {
+
+    static long[] dp = new long[101];
+
+    static long fun(int n) {
+
+
+        dp[0] = dp[1] = dp[2] = dp[3] = 1;
+        dp[4] = dp[5] = 2;
+
+        for(int i = 6; i<n+1; i++) {
+            dp[i] = dp[i-5] + dp[i-4] + dp[i-3];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 1; tc <T+1; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            System.out.println(fun(N));
+        }
+
+    }
+
+}
+
