7035b2323e198985a698e14b8f22ae21646cc94a
gamin <gamin3182@gmail.com>
2025-09-23 22:22:20 +0900

    8/10 SWEA 2001

---
+++ src/week2/s_2001.java
@@ -0,0 +1,54 @@
+package week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class s_2001 {
+
+    static int[][] graph;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i<T+1; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            graph = new int[N][N];
+            int max_value = 0;
+
+            for (int j = 0; j<N; j++){
+                st = new StringTokenizer(br.readLine());
+
+                for(int k = 0; k<N; k++){
+                    graph[j][k] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+
+//            int i1 = ((N - M) + 1) * ((N - M) + 1);
+            for(int x = 0; x <= N-M; x ++){
+                for (int y =0; y<= N-M; y++){
+                    int sum = 0;
+                    for(int l = 0; l<M; l++){
+                        for(int m =0; m<M; m++){
+                            sum += graph[x+l][y+m];
+                        }
+                    }
+                    if (sum > max_value) max_value = sum;
+                }
+            }
+
+
+
+            System.out.println("#" + i + " " + max_value);
+
+        }
+    }
+}
