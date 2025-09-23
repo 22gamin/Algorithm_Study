2e58d0ea16f5e2eae7bf10342d1ae4571ce7e99c
gamin <gamin3182@gmail.com>
2025-09-23 23:02:09 +0900

    9/11 BOJ 1793

---
+++ src/week7/b_1793.java
@@ -0,0 +1,36 @@
+package week7;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class b_1793 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String line = br.readLine();
+            if ( line == null) break;
+            int n = Integer.parseInt(line);
+
+            BigInteger[] dp = new BigInteger[251];
+
+
+            dp[0] = new BigInteger("1");
+            dp[1] = new BigInteger("1");
+            dp[2] = new BigInteger("3");
+            dp[3] = new BigInteger("5");
+
+
+            for(int i = 4; i<n+1; i++) {
+                dp[i] = dp[i-2].multiply(BigInteger.valueOf(2)).add(dp[i-1]);
+            }
+
+            System.out.println(dp[n]);
+        }
+
+    }
+
+}
+
