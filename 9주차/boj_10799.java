f52506d4f0405978516c3926c81f46ceb8536597
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-09-25 00:26:10 +0900

    9주차/김민종/3문제

---
+++ 9주차/boj_10799.java
@@ -0,0 +1,32 @@
+package week_9;
+
+import java.util.*;
+import java.io.*;
+
+public class boj_10799 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        Deque<Character> stack = new ArrayDeque<>();
+        int ans = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char cur = str.charAt(i);
+
+            if (cur == '(') {
+                stack.push(cur);
+            } else {
+                stack.pop();
+
+                if (str.charAt(i - 1) == '(') {
+                    ans += stack.size();
+                } else {
+                    ans += 1;
+                }
+            }
+
+        }
+        System.out.println(ans);
+    }
+}
