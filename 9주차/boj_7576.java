c2b3417d9e42e5665370253a7106067f3ca87a9e
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-09-28 10:18:36 +0900

    9주차/김민종/5문제

---
+++ 9주차/boj_7576.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class boj_7576 {
+    static int[] dx = { 0, 0, -1, 1 };
+    static int[] dy = { -1, 1, 0, 0 };
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
+                    q.offer(new int[] { i, j });
+                }
+            }
+        }
+
+        // bfs
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int y = cur[0];
+            int x = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextY = y + dy[i];
+                int nextX = x + dx[i];
+
+                if (nextY >= 0 && nextY < N && nextX >= 0 && nextX < M && arr[nextY][nextX] == 0) {
+                    arr[nextY][nextX] = arr[y][x] + 1;
+                    q.offer(new int[] { nextY, nextX });
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+
+        System.out.println(max - 1);
+    }
+}
