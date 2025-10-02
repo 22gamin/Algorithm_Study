c94679c7118c50e3e4fa8cec4fd9b13154487048
JangGaEun <wkdrkdms0101@gmail.com>
2025-10-02 12:37:51 +0900

    10.02 코드 업로드

---
+++ src/week9/B9465.java
@@ -0,0 +1,46 @@
+package week9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B9465 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 0; t < T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] sticker = new int[2][N];
+
+			for (int i = 0; i < 2; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					sticker[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			if (N == 1) {
+				sb.append(Math.max(sticker[0][0], sticker[1][0])).append("\n");
+				continue;
+			}
+
+			int[][] dp = new int[2][N];
+			dp[0][0] = sticker[0][0];
+			dp[1][0] = sticker[1][0];
+			dp[0][1] = dp[1][0] + sticker[0][1];
+			dp[1][1] = dp[0][0] + sticker[1][1];
+
+			for (int i = 2; i < N; i++) {
+				dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + sticker[0][i];
+				dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + sticker[1][i];
+			}
+
+			sb.append(Math.max(dp[0][N - 1], dp[1][N - 1])).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
