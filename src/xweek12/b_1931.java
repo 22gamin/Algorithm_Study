beeefbea3e4e7983f63a426a96f153c8fef0cc36
gamin <108453365+22gamin@users.noreply.github.com>
2025-10-16 10:16:39 +0900

    10/16 BOJ 1931

---
+++ src/xweek12/b_1931.java
@@ -0,0 +1,62 @@
+package test;
+
+import java.util.*;
+import java.io.*;
+
+public class b_1931 {
+	static int N;
+	static List<Meeting> meetings;
+	
+	static class Meeting implements Comparable<Meeting>{
+		int start; 
+		int end;
+		
+		public Meeting(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+		
+		@Override
+		public int compareTo(Meeting o) {
+			if (this.end == o.end) {
+				return this.start - o.start;
+			}
+			return this.end - o.end;
+		}
+		
+		@Override
+		public String toString() {
+			return "[" + this.start + "," + this.end + "]";
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		meetings = new ArrayList<>();
+		
+		for(int n = 0; n<N; n++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			meetings.add(new Meeting(start,end));
+		}
+		Collections.sort(meetings);
+		
+		int end = 0;
+		int cnt = 0;
+		
+		for(int i = 0; i<N; i++) {
+			if (end <= meetings.get(i).start) {
+				end = meetings.get(i).end;
+				cnt++;
+
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
