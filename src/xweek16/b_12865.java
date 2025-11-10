6167a576bafb6f954fb8a0e3c5cf4e982fec8fda
gamin <108453365+22gamin@users.noreply.github.com>
2025-11-10 15:57:35 +0900

    11/19 BOJ 12865

---
+++ src/xweek16/b_12865.java
@@ -0,0 +1,35 @@
+package xweek16;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class b_12865 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 물품의 수
+        int K = Integer.parseInt(st.nextToken()); // 버틸 수 있는 무게
+
+        // dp[j] = 무게 한도가 j일 때의 최대 가치
+        int[] dp = new int[K + 1];
+
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken()); // 현재 물건의 무게
+            int V = Integer.parseInt(st.nextToken()); // 현재 물건의 가치
+
+            for (int j = K; j >= W; j--) {
+                
+
+                dp[j] = Math.max(dp[j], dp[j - W] + V);
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
+}
